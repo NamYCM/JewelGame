c2284c0496482dd04aa0a8a44535463c51baf61b
Hai Nam <hnam19052000@gmail.com>
2022-06-06 11:50:08 +0700

    build window platform

--- Assets/Modern UI Pack/Scripts/UI Manager/UIManagerProgressBar.cs
+++ Assets/Modern UI Pack/Scripts/UI Manager/UIManagerProgressBar.cs
@@ -20,9 +20,9 @@ namespace Michsky.UI.ModernUIPack
 
         void Awake()
         {
-#if UNITY_WEBGL
-            webglMode = true;
-#endif
+// #if UNITY_WEBGL
+//             webglMode = true;
+// #endif
             if (Application.isPlaying && webglMode == true)
                 return;
 
